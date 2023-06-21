0fdc079bf32c5eea3cd35db1db7226cc473cc8ff
Resmart <90556532+Resmart1@users.noreply.github.com>
2023-06-21 17:55:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let socket = new WebSocket("wss://server-resmart.onrender.com/");
+
+// отправка сообщения из формы
+document.forms.publish.onsubmit = function () {
+	let outgoingMessage = this.message.value;
+
+	socket.send(JSON.stringify(outgoingMessage));
+	return false;
+};
+
+// получение сообщения - отобразить данные в div#messages
+socket.onmessage = (event) => {
+	let message = JSON.parse(event.data);
+	message.forEach(messages => {
+		let messageElem = document.createElement('div');
+		messageElem.textContent = messages;
+		document.getElementById('messages').prepend(messageElem);
+	})
+}
